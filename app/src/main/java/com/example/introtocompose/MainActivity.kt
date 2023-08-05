bd75743a14962573ee70b6cb995f9b6ba5a09216
Tamzid Israk <tamzidisrak12@gmail.com>
2023-08-06 01:41:18 +0600

    Intro to jetpack compose 1.Row and Column 2.Modifier 3.Lazy Column 4.CircularImage 5.side effect of jetpack compose -> launchedEffect, rememberCoroutineScope,rememberUpdateState,DisposableSideEffect

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.introtocompose
 
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewTreeObserver
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -35,6 +36,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -42,15 +44,19 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           DisposableExample()
+         KeyboardComposable()
+         TextField(value = "", onValueChange = {})
         }
     }
 }
@@ -58,11 +64,12 @@ class MainActivity : ComponentActivity() {
 
 //text in compose
 @Composable
-fun sayCheeze(name:String="Cheezzy "){
-    Text(text = "hello $name",
+fun sayCheeze(name: String = "Cheezzy ") {
+    Text(
+        text = "hello $name",
         fontStyle = FontStyle.Italic,
         fontWeight = FontWeight.ExtraBold,
-        color= Color.Red,
+        color = Color.Red,
         textAlign = TextAlign.Start
     )
 }
@@ -70,8 +77,9 @@ fun sayCheeze(name:String="Cheezzy "){
 
 //image in compose
 @Composable
-private fun previewImage(){
-    Image(painter = painterResource(id = R.drawable.mobil),
+private fun previewImage() {
+    Image(
+        painter = painterResource(id = R.drawable.mobil),
         contentDescription = "Description Image",
 //        colorFilter = ColorFilter.tint(Color.Green)
 //        contentScale = ContentScale.Crop
@@ -81,15 +89,19 @@ private fun previewImage(){
 
 //button with click in compose
 @Composable
-private fun btnfunc(){
+private fun btnfunc() {
 
-    Button(onClick = {},
+    Button(
+        onClick = {},
         colors = ButtonDefaults.buttonColors(
 //            containerColor = Color.White,
-        )) {
+        )
+    ) {
         Text(text = "Click Btn")
-        Image(painter = painterResource(id = R.drawable.add),
-            contentDescription = "Dummy")
+        Image(
+            painter = painterResource(id = R.drawable.add),
+            contentDescription = "Dummy"
+        )
     }
 
 }
@@ -98,25 +110,26 @@ private fun btnfunc(){
 //edittext in compose
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun TextPrev(){
+private fun TextPrev() {
     //state will reserve the value everytime after changes
     val state = remember {
         mutableStateOf("")
     }
-    TextField(value = state.value,
+    TextField(
+        value = state.value,
         onValueChange = {
-            state.value=it
+            state.value = it
         },
-        label = { Text(text = "Enter Message")},
-        placeholder = { Text(text = "The Name")},
+        label = { Text(text = "Enter Message") },
+        placeholder = { Text(text = "The Name") },
 
-    )
+        )
 }
 
 
 //row and col in compose
 @Composable
-private fun rowAndColPrev(){
+private fun rowAndColPrev() {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -134,13 +147,13 @@ private fun rowAndColPrev(){
 }
 
 
-
 //list in compose
 @Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-private fun listView(){
+private fun listView() {
     Row(Modifier.padding(8.dp)) {
-        Image(painter = painterResource(id = R.drawable.user),
+        Image(
+            painter = painterResource(id = R.drawable.user),
             contentDescription = "",
             Modifier.size(40.dp)
         )
@@ -149,9 +162,11 @@ private fun listView(){
                 text = "Adol",
                 fontWeight = FontWeight.Bold
             )
-            Text(text = "Android Developer",
+            Text(
+                text = "Android Developer",
                 fontWeight = FontWeight.ExtraLight,
-                fontSize = 15.sp)
+                fontSize = 15.sp
+            )
         }
     }
 }
@@ -160,7 +175,7 @@ private fun listView(){
 //modifier in compose
 @Preview(showBackground = true, widthDp = 300, heightDp = 500)
 @Composable
-private fun modifyFun(){
+private fun modifyFun() {
     Text(text = "Modifier",
         color = Color.White,
         modifier = Modifier
@@ -170,31 +185,33 @@ private fun modifyFun(){
             .border(4.dp, Color.Red)
             .clip(CircleShape)
             .background(Color.Yellow)
-        )
+    )
 }
 
 
 //circular image in jetpack
 @Composable
-private fun circularImage(){
-    Image(painter = painterResource(id = R.drawable.mobil),
+private fun circularImage() {
+    Image(
+        painter = painterResource(id = R.drawable.mobil),
         contentDescription = "mobile",
         contentScale = ContentScale.Fit,
         modifier = Modifier
             .size(100.dp)
             .clip(CircleShape)
-            .border(2.dp, Color.Black, CircleShape))
+            .border(2.dp, Color.Black, CircleShape)
+    )
 }
 
 //side effects in compose
 //if we need any function to execute only one time we can use launchedEffect(_)
 
 @Composable
-private fun Counter(){
-    var counter = remember{ mutableStateOf(0) }
-    var key = counter.value%3==0  // it will change the value of key
-    LaunchedEffect(key1 = key,){  //it will run for only 1 time unless the value of is key is changed
-        Log.d("count","${counter.value}")
+private fun Counter() {
+    var counter = remember { mutableStateOf(0) }
+    var key = counter.value % 3 == 0  // it will change the value of key
+    LaunchedEffect(key1 = key) {  //it will run for only 1 time unless the value of is key is changed
+        Log.d("count", "${counter.value}")
     }
     Button(onClick = { counter.value++ }) {
         Text(text = "increment")
@@ -202,34 +219,33 @@ private fun Counter(){
 }
 
 
-
 //remember coroutineScope
 @Composable
-private fun LaunchedEffectComposable(){
-    var count= remember {
+private fun LaunchedEffectComposable() {
+    var count = remember {
         mutableStateOf(0)
     }
 
     var scope = rememberCoroutineScope()  // it can only use in launchedEffect or event like button
 
-    LaunchedEffect(key1 = Unit){
+    LaunchedEffect(key1 = Unit) {
         scope.launch {
-            Log.d("count","Started")
+            Log.d("count", "Started")
             try {
-                for (i in 1..10){
+                for (i in 1..10) {
                     count.value++
                     delay(1000)
                 }
-            }catch (e:Exception){
+            } catch (e: Exception) {
                 Log.d("count", e.toString())
             }
         }
     }
-    
+
 
     var text = "counter is running ${count.value}"
-    if (count.value==10){
-        text="Counter stopped"
+    if (count.value == 10) {
+        text = "Counter stopped"
     }
 
     Text(text = text)
@@ -237,23 +253,23 @@ private fun LaunchedEffectComposable(){
 
 //RememberUpdateState
 @Composable
-private fun App(){
-    var count= remember {
+private fun App() {
+    var count = remember {
         mutableStateOf(0)
     }
-    LaunchedEffect(key1 = Unit){
+    LaunchedEffect(key1 = Unit) {
         delay(2000)
-        count.value=10
+        count.value = 10
     }
     Counter(value = count.value)
 }
 
 @Composable
-private fun Counter(value:Int){
+private fun Counter(value: Int) {
     val state = rememberUpdatedState(newValue = value) //it will remember the value of updated state
-    LaunchedEffect(key1 = Unit){
+    LaunchedEffect(key1 = Unit) {
         delay(5000)
-        Log.d("count",state.value.toString())
+        Log.d("count", state.value.toString())
     }
 
     Text(text = value.toString())
@@ -262,22 +278,39 @@ private fun Counter(value:Int){
 
 //DISPOSABLE EFFECT
 @Composable
-private fun DisposableExample(){
-    var state= remember {
+private fun DisposableExample() {
+    var state = remember {
         mutableStateOf(false)
     }
 
-    DisposableEffect(key1 = state.value,){
-        Log.d("count","Disposable effect started")
+    DisposableEffect(key1 = state.value) {
+        Log.d("count", "Disposable effect started")
         onDispose {
-            Log.d("count","Cleaning up items")
+            Log.d("count", "Cleaning up items")
         }
     }
 
     Button(onClick = {
-        state.value=!state.value
+        state.value = !state.value
     }) {
         Text(text = "changeState")
+    }
+}
+
+@Composable
+private fun KeyboardComposable() {
+    val view = LocalView.current
+    DisposableEffect(key1 = Unit) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val insets = ViewCompat.getRootWindowInsets(view)
+            val isKeyboardVisible = insets?.isVisible(WindowInsetsCompat.Type.ime())
+            Log.d("count", isKeyboardVisible.toString())
+        }
+        view.viewTreeObserver.addOnGlobalLayoutListener(listener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
     }
 }
 
